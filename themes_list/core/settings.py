361bb4d3de46030781653fa2803e2badb272d119
RGBsrs <grjanko.d.a@gmail.com>
2021-12-05 10:54:58 +0200

    update settings

--- themes_list/core/settings.py
+++ themes_list/core/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-xlok&3_zpgy+1#b*n$48m&twel!+t+gn&_f%#7+it2@_8hkt(p'
+SECRET_KEY = 'django-insecure-xlok&3_zpgy+1#b*n$48m&twel!+t+gn&_f%#7+it2@_8hkt(p'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
