06086ecef50e2fbaa23d69c663f735b4bca94260
RGBsrs <grjanko.d.a@gmail.com>
2021-12-05 10:44:51 +0200

    add heroku settings

--- themes_list/core/settings.py
+++ themes_list/core/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-xlok&3_zpgy+1#b*n$48m&twel!+t+gn&_f%#7+it2@_8hkt(p'
+# SECRET_KEY = 'django-insecure-xlok&3_zpgy+1#b*n$48m&twel!+t+gn&_f%#7+it2@_8hkt(p'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -126,3 +126,6 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+import django_heroku
+django_heroku.settings(locals())
--- themes_list/themes_list/views.py
+++ themes_list/themes_list/views.py
@@ -5,6 +5,20 @@ from django.template.loader import get_template
 import pdfkit
 
 from .models import Theme
+
+import os
+import sys
+import subprocess
+import platform
+
+if platform.system() == "Windows":
+    pdfkit_config = pdfkit.configuration(wkhtmltopdf=os.environ.get(
+        'WKHTMLTOPDF_BINARY', 'C:\\Program Files\\wkhtmltopdf\\bin\\wkhtmltopdf.exe'))
+else:
+    os.environ['PATH'] += os.pathsep + os.path.dirname(sys.executable)
+    WKHTMLTOPDF_CMD = subprocess.Popen(['which', os.environ.get('WKHTMLTOPDF_BINARY', 'wkhtmltopdf')],
+                                        stdout=subprocess.PIPE).communicate()[0].strip()
+    pdfkit_config = pdfkit.configuration(wkhtmltopdf=WKHTMLTOPDF_CMD)
 # Create your views here.
 class ThemeHome(ListView):
     model = Theme
@@ -37,7 +51,7 @@ def generate_pdf(request):
     }
 
     # генерация файла
-    pdf = pdfkit.from_string(html, False, options)
+    pdf = pdfkit.from_string(html, False, options, configuration=pdfkit_config)
 
     # формирования ответа сервера
     response = HttpResponse(pdf, content_type='application/pdf')
